f815e8138d5af1066c154497a7c543c57e0e1946
huanyu <huanyu.li@iaac.net>
2025-05-02 12:37:20 +0200

    update handeye and robot driver

--- .docker/build_image.sh
+++ .docker/build_image.sh
@@ -4,5 +4,5 @@ echo -e "Building image moveit2_introduction:latest"
 DOCKER_BUILDKIT=1 \
 docker build --pull --rm -f ./.docker/Dockerfile \
 --build-arg BUILDKIT_INLINE_CACHE=1 \
---target base \
+--target build \
 --tag moveit2_introduction:latest .
